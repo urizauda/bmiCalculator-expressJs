9bd15b0504d0d426a091a38413c9dfd085304389
Uri Zauda <87427105+urizauda@users.noreply.github.com>
2022-12-26 15:12:10 +0200

    post method

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8080;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/")
+})
+
+app.post("/", function(req, res) {
+    let weight = parseFloat(req.body.weight);
+    let height = parseFloat(req.body.height);
+    let bmi = weight / (height * height);
+    res.send(`Your BMI is: ${bmi}`)
+})
+
+
+
+app.listen(PORT, (() => { console.log(`server listening on port ${PORT}`); }))
